1447bcb85cbc52855ee1e7fc2ff286c84c4f8e9b
chika-12 <markworship001@gmail.com>
2026-04-30 05:02:25 +0100

    feature(scope):changed base url

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://hngintern-production-7bfd.up.railway.app/api/v1';
+const BASE_URL = 'https://hngstage3-production.up.railway.app/api/v1';
 
 function login() {
   window.location.href = `${BASE_URL}/auth/github`;
--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://hngintern-production-7bfd.up.railway.app/api/v1';
+const BASE_URL = 'https://hngstage3-production.up.railway.app/api/v1';
 
 // ── State ──────────────────────────────────────────────────────────────────
 let state = {
